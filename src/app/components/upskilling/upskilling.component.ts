4de1d2302c81f57836916426856b81a5a9c74d4d
Terry <chittert@gmail.com>
2022-10-24 15:01:18 +0200

    added functionality to upskilling

--- src/app/components/upskilling/upskilling.component.ts
+++ src/app/components/upskilling/upskilling.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UpskillingService } from 'src/app/services/upskilling.service';
 
 @Component({
   selector: 'app-upskilling',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upskilling.component.css']
 })
 export class UpskillingComponent implements OnInit {
+  topics: any;
+  openTopic: any;
 
-  constructor() { }
+  constructor(private upskillData: UpskillingService) {
+    this.topics = upskillData.getTopics();
+    this.openTopic = this.topics[0];
+  }
 
   ngOnInit(): void {
+  }
+
+  setOpenTopic(topic:string){
+    this.openTopic = this.topics[this.topics.findIndex((n: { name: string; }) => n.name == topic)];
   }
 
 }
